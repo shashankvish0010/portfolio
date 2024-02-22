03cbbecd781960268867be616d3c66982521e4ce
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-02-22 17:51:46 +0530

    hedaer added

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 const App: React.FC = () => {
   return (
     <>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
       </Routes>
