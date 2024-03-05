992a7c902482c7cf15391f572b638316e8a9e125
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-03-05 19:55:35 +0530

    Updated: typing animation config

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -15,15 +15,7 @@ export default {
           "100%": {
             width: "100%"
           }  
-        },
-        // blink: {
-        //   "50%": {
-        //     borderColor: "transparent"
-        //   },
-        //   "100%": {
-        //     borderColor: "blue"
-        //   }  
-        // }
+        }
       },
       animation: {
         typing: "typing 2s steps(20) alternate infinite, blink .7s"
