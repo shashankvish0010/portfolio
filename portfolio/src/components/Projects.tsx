f77c7a4d2f7c66a336e5c5bc3958b3282b5b4e58
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-03-15 01:42:10 +0530

    portfolio updated

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -42,11 +42,11 @@ const Projects: React.FC<CardType> = (props: CardType) => {
                         }
                     </div>
                     <div className='h-max w-max flex gap-3'>
-                        <span onClick={() => window.location.href = props.projectUrl} className='header_list h-max w-max flex flex-row items-center gap-2 bg-indigo-600 text-white p-2 rounded'>
+                        <span onClick={() => window.location.href = props.projectUrl} className='cursor-pointer header_list h-max w-max flex flex-row items-center gap-2 bg-indigo-600 text-white p-2 rounded'>
                             <p>Live Preview</p>
                             <Icon icon="tabler:arrow-up" color='white' height={'2rem'} />
                         </span>
-                        <span onClick={() => window.location.href = props.github} className='header_list h-max w-max flex flex-row items-center gap-2 bg-slate-800 text-white p-2 rounded'>
+                        <span onClick={() => window.location.href = props.github} className='cursor-pointer header_list h-max w-max flex flex-row items-center gap-2 bg-slate-800 text-white p-2 rounded'>
                             <p>GitHub</p>
                             <Icon icon="flowbite:github-solid" style={{ color: 'white' }} height={'2rem'} />
                         </span>
