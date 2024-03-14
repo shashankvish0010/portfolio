0d1b53ffef4fe57d9316bf6522c8e12037da01dc
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-03-14 18:50:45 +0530

    Changes: Ui for projects section.

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -10,13 +10,13 @@ interface CardType {
 const Projects: React.FC<CardType> = (props: CardType) => {
     return (
         <div className='h-max w-[100%] flex flex-col items-center gap-5'>
-            <div className='h-max w-[80%]'>
+            <div className='h-max md:w-[80%] w-[90%] justify-start'>
                 <p className='header_list md:text-3xl text-2xl'>{props.project_name}</p>
             </div>
             <div className='bg-gradient-to-r from-indigo-600 via-violet-500 to-pink-500 p-5 rounded-xl border-2 shadow-xl md:flex hidden justify-center items-center h-max w-max'>
                 <iframe width="840" height="473" src={props.loomVideo} className='border-2 rounded-xl shadow-lg' allowFullScreen></iframe>
             </div>
-            <div className='md:hidden flex justify-center items-center h-max w-max'>
+            <div className='bg-gradient-to-r from-indigo-600 via-violet-500 to-pink-500 p-3 rounded-xl border-2 shadow-xl md:hidden flex justify-center items-center h-max w-max'>
                 <iframe width="360" height="203" src={props.loomVideo} className='border-2 rounded-xl shadow-lg' allowFullScreen></iframe>
             </div>
             <div className='flex md:flex-row flex-col md:items-center justify-center gap-5 p-3 w-[90%]'>
