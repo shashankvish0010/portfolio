b5c0a839a912d1f70e3489fddb1d4823967ecf4d
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-03-14 23:44:38 +0530

    Updated: Calendly booking url.

--- portfolio/src/components/Calendly.tsx
+++ portfolio/src/components/Calendly.tsx
@@ -5,12 +5,12 @@ const Calendly: React.FC = () => {
 
     return (
         <>
-                <div className='md:block hidden h-max w-screen p-5'>
-            <InlineWidget styles={{height: "700px"}} url='https://calendly.com/shashankvish010/30min' />
-        </div>
-        <div className='md:hidden block h-max w-screen p-5'>
-            <InlineWidget styles={{width: "90vw", height: "900px"}} url='https://calendly.com/shashankvish010/30min' />
-        </div>
+            <div className='md:block hidden h-max w-screen p-5'>
+                <InlineWidget styles={{ height: "700px" }} url='https://calendly.com/shashankvish010/casual-conversation' />
+            </div>
+            <div className='md:hidden block h-max w-screen p-5'>
+                <InlineWidget styles={{ width: "90vw", height: "900px" }} url='https://calendly.com/shashankvish010/casual-conversation' />
+            </div>
         </>
     )
 }
